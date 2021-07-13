805e335c6414d00de13c149a1e1c0ece07c067d1
Joshua Hull <tarnillion@gmail.com>
2021-07-13 12:19:49 -0500

    Add project files.

---
+++ UDP_Arduino/UDPClient.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace UDP_Arduino
+{
+
+    public struct Received
+    {
+        public IPEndPoint Sender;
+        public string Message;
+    }
+
+    abstract class UdpBase
+    {
+        protected UdpClient Client;
+
+        protected UdpBase()
+        {
+            Client = new UdpClient();
+        }
+
+        public async Task<Received> Receive()
+        {
+            var result = await Client.ReceiveAsync();
+            return new Received()
+            {
+                Message = Encoding.ASCII.GetString(result.Buffer, 0, result.Buffer.Length),
+                Sender = result.RemoteEndPoint
+            };
+        }
+    }
+
+    //Server
+    class UdpListener : UdpBase
+    {
+        private IPEndPoint _listenOn;
+
+        public UdpListener() : this(new IPEndPoint(IPAddress.Any, 32123))
+        {
+        }
+
+        public UdpListener(IPEndPoint endpoint)
+        {
+            _listenOn = endpoint;
+            Client = new UdpClient(_listenOn);
+        }
+
+        public void Reply(string message, IPEndPoint endpoint)
+        {
+            var datagram = Encoding.ASCII.GetBytes(message);
+            Client.Send(datagram, datagram.Length, endpoint);
+        }
+
+    }
+
+    //Client
+    class UdpUser : UdpBase
+    {
+        private UdpUser() { }
+
+        public static UdpUser ConnectTo(string hostname, int port)
+        {
+            var connection = new UdpUser();
+            connection.Client.Connect(hostname, port);
+            return connection;
+        }
+
+        public void Send(byte[] message)
+        {
+            var datagram = message;
+            //var datagram = Encoding.ASCII.GetBytes(message);
+            Client.Send(datagram, datagram.Length);
+        }
+        public void Send(string message)
+        {
+            var datagram = Encoding.ASCII.GetBytes(message);
+            Client.Send(datagram, datagram.Length);
+        }
+    }
+
+
+
+    public class UDPClient
+    {
+        UdpUser client;
+        public UDPClient()
+        {
+            client = UdpUser.ConnectTo("192.168.1.167", 2390);
+        }
+
+        public bool Send(uint m1, uint m2, uint m3, uint m4, uint m5, uint m6, uint step)
+        {
+            bool cmd = false;
+            Task.Factory.StartNew(async () =>
+            {
+                byte[] msg = {
+                Convert.ToByte(0),
+                Convert.ToByte(step),
+                Convert.ToByte(m1),
+                Convert.ToByte(m2),
+                Convert.ToByte(m3),
+                Convert.ToByte(m4),
+                Convert.ToByte(m5),
+                Convert.ToByte(m6)
+                 };
+                client.Send(msg);
+                var received = await client.Receive();
+
+                if (received.Message.Contains("acknowledged"))
+                {
+                    cmd = true;
+                }
+            }
+            );
+            return true;
+        }
+        
+
+
+    }
+
+
+}
---
+++ gRPC_TEST1/Program.cs
@@ -0,0 +1,115 @@
+﻿using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityComm;
+using UDP_Arduino;
+
+namespace gRPC_TEST1
+{
+    //class GreeterImpl : Greeter.GreeterBase
+    //{
+    //    // Server side handler of the SayHello RPC
+    //    public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+    //    {
+    //        return Task.FromResult(new HelloReply { Message = "Hello Grande " + request.Name });
+    //    }
+    //}
+
+    class Program
+    {
+        const int ListenPort = 30051;
+
+        static void Main(string[] args)
+        {
+
+
+
+            Server server = new Server
+            {
+                Services = { ArmComm.BindService(new ArmCommImpl()) },
+                Ports = {new ServerPort("localhost", ListenPort, ServerCredentials.Insecure)}
+            };
+
+            //Server server = new Server
+            //{
+            //    Services = { Greeter.BindService(new GreeterImpl()) },
+            //    Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
+            //};
+            server.Start();
+
+            Console.WriteLine("Greeter server listening on port " + ListenPort);
+
+
+
+            string read;
+            do
+            {
+
+
+                read = Console.ReadLine();
+
+                //client.Send(read);
+            } while (read != "quit");
+
+
+        }
+    }
+
+    class ArmCommImpl : ArmComm.ArmCommBase
+    {
+        // Server side handler of the SayHello RPC
+        ServoValues _lastRequest;
+
+        uint m1;
+        uint m2;
+        uint m3;
+        uint m4;
+        uint m5;
+        uint m6;
+
+        private bool NotDuplicate(ServoValues request)
+        {
+            return (m1 != request.M1 || m2 != request.M2 || m3 != request.M3 || m4 != request.M4 || m5 != request.M5 || m6 != request.M6);
+        }
+
+        //Establish UDP Client to handle requests to arduino via IP/UDP
+        UDPClient UDPClientArduino = new UDPClient();
+
+        public override Task<Recieved> SendArmComm(ServoValues request, ServerCallContext context)
+        {
+            bool finishedCmd = false;
+
+            if (NotDuplicate(request))
+            {
+                m1 = request.M1;
+                m2 = request.M2;
+                m3 = request.M3;
+                m4 = request.M4;
+                m5 = request.M5;
+                m6 = request.M6;
+                //Console.WriteLine("message recieved" + request.ToString());
+
+                //Add calls to Arduino via UDP
+                var reply = UDPClientArduino.Send(m1, m2, m3, m4, m5, m6, request.Step);
+                if (reply)
+                {
+                    finishedCmd = true;
+                    _lastRequest = request;
+                }
+            }
+
+            return Task.FromResult(new Recieved { Replymessage = finishedCmd }); ;
+        }
+        
+
+        
+        //    public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        //    {
+        //        return Task.FromResult(new HelloReply { Message = "Hello Grande " + request.Name });
+        //    }
+
+    }
+}
